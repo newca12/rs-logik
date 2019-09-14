b56dfd8fc988c95f83eae5e07bfc94e9d3030ae1
Nathan Graule <solarliner@gmail.com>
2019-09-14 19:25:24 +0200

    WIP solver, implement or distribution function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod eval;
 mod parse;
+mod solve;
 mod util;
 pub use eval::{eval, evaluate_ast};
 pub use parse::{Node, Parser, Token};
---
+++ src/solve.rs
@@ -0,0 +1,74 @@
+use crate::Node;
+
+pub fn distribute_or<'s>(ast: Box<Node<'s>>) -> Option<Node<'s>> {
+    match *ast {
+        Node::BinOpNode("->", _, _) => None,
+        Node::BinOpNode("ou", expr_a, expr_b) => {
+            if let Node::BinOpNode("et", expr_c, expr_d) = *expr_b {
+                let left = distribute_or(Box::new(Node::BinOpNode(
+                    "ou",
+                    Box::new(distribute_or(expr_a.clone())?),
+                    Box::new(distribute_or(expr_c)?),
+                )))?;
+                let right = distribute_or(Box::new(Node::BinOpNode(
+                    "ou",
+                    Box::new(distribute_or(expr_a)?),
+                    Box::new(distribute_or(expr_d)?),
+                )))?;
+                Some(Node::BinOpNode("et", Box::new(left), Box::new(right)))
+            } else if let Node::BinOpNode("et", expr_c, expr_d) = *expr_a {
+                let left = distribute_or(Box::new(Node::BinOpNode(
+                    "ou",
+                    Box::new(distribute_or(expr_c)?),
+                    Box::new(distribute_or(expr_b.clone())?),
+                )))?;
+                let right = distribute_or(Box::new(Node::BinOpNode(
+                    "ou",
+                    Box::new(distribute_or(expr_d)?),
+                    Box::new(distribute_or(expr_b)?),
+                )))?;
+                Some(Node::BinOpNode("et", Box::new(left), Box::new(right)))
+            } else {
+                Some(Node::BinOpNode(
+                    "ou",
+                    Box::new(distribute_or(expr_a)?),
+                    Box::new(distribute_or(expr_b)?),
+                ))
+            }
+        }
+        Node::BinOpNode(op, left, right) => Some(Node::BinOpNode(
+            op,
+            Box::new(distribute_or(left)?),
+            Box::new(distribute_or(right)?),
+        )),
+        Node::UnopNode(_, right) => Some(*right),
+        Node::ExprNode(expr) => distribute_or(expr),
+        _ => Some(*ast),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::distribute_or;
+    use super::Node;
+    use crate::eval::pprint;
+
+    #[test]
+    fn test_distribute_or() {
+        let input_node = Node::BinOpNode(
+            "ou",
+            Box::new(Node::ExprNode(Box::new(Node::BinOpNode(
+                "et",
+                Box::new(Node::UnopNode("non", Box::new(Node::IdentNode("a")))),
+                Box::new(Node::IdentNode("c")),
+            )))),
+            Box::new(Node::IdentNode("b")),
+        );
+        println!(
+            "{} -> {}",
+            pprint(&input_node),
+            pprint(&distribute_or(Box::new(input_node)).expect("Couldn't distribute or"))
+        );
+        assert!(false);
+    }
+}
